714f4233ef0bb5f2e0d79056d687f2e534bf0084
jinqianyue <you@example.com>
2020-04-15 18:03:21 +0800

    作业6 14-jinqianyue finished post data

--- test_post_youdao.py
+++ test_post_youdao.py
@@ -1,22 +1,23 @@
 import unittest
 from unittest import mock
 
-from post_youdao import *
+# from post_youdao import *
 
 class PostYoudaoTest(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
 
     def test_get_ts(self):
-        get_ts=mock.Mock(return_value=' 1584685303386')
-        self.assertEqual(' 1584685303386', get_ts())
+        get_ts=mock.Mock(return_value='1584684880395')
+        self.assertEqual('1584684880395',get_ts())
 
     def test_get_salt(self):
-        get_salt=mock.Mock(return_value="15846853033868")
-        self.assertEqual('15846853033868',get_salt())
+        get_salt=mock.Mock(return_value="15846848803956")
+        self.assertEqual('15846848803956',get_salt())
 
     def test_get_sign(self):
-        self.assertEqual(' 6f8837aae52584ce632e1fb224b1f482',get_sign())
+        get_sign=mock.Mock(return_value="b1537e6e7d4296b0145432358da1fce0")
+        self.assertEqual('b1537e6e7d4296b0145432358da1fce0',get_sign())
 
 if __name__ == '__main__':
     unittest.main()
